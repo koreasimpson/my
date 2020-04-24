9dd84b898f9a7ac435b4c37caeece8984e56f087
김찬호 <90chanho@gmail.com>
2020-04-25 03:09:30 +0900

    modify path

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import "./common.js"
-
 import "../scss/main.scss"
 
 import myImg from "../assets/img/profile.JPG"
 
 document.addEventListener("DOMContentLoaded", function() {
 	const profileImg = document.querySelector(".profile img")
-	profileImg.setAttribute("src", myImg)
+	console.log("myImg =", myImg)
+	profileImg.setAttribute("src", `my/${myImg}`)
 })
